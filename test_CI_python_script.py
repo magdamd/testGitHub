62a56e33e4e3098bc4fc4b8c4b7fabe4ae01a822
magdamd <49106367+magdamd@users.noreply.github.com>
2024-04-08 18:01:21 +0200

    Update test_CI_python_script.py - correct value for subtract

--- test_CI_python_script.py
+++ test_CI_python_script.py
@@ -6,4 +6,4 @@ class TestPythonScript:
         assert 4 == CI_CD_test_python_script.add(2, 2)
 
     def test_subtraction(self):
-        assert 1 == CI_CD_test_python_script.subtract(4, 2)
+        assert 2 == CI_CD_test_python_script.subtract(4, 2)

6b08f2d5b145be0528552b0f364b1032799dac33
magdamd <49106367+magdamd@users.noreply.github.com>
2024-04-08 23:39:30 +0200

    Update CI_CD_test_python_script.py - correct writing mode

--- CI_CD_test_python_script.py
+++ CI_CD_test_python_script.py
@@ -3,11 +3,17 @@ import numpy as np
 def add(a, b):
   r = a + b
   #with open("New_version_values.txt", "ab") as f:
-  np.savetxt("New_version_values.txt", np.asarray(r), fmt="%1.4e")#'test.out', x, fmt='%1.4e'
+  f = open("New_version_values.txt", 'a')
+  f.write(str(r))
+  f.close()
+  #np.savetxt("New_version_values.txt", np.asarray(r), fmt="%1.4e")#'test.out', x, fmt='%1.4e'
   return r
 
 def subtract(a, b):
   r = a - b
   #with open("New_version_values.txt", "ab") as f:
-  np.savetxt("New_version_values.txt", np.asarray(r), fmt="%1.4e")
+  #np.savetxt("New_version_values.txt", np.asarray(r), fmt="%1.4e")
+  f = open("New_version_values.txt", 'a')
+  f.write(str(r))
+  f.close()
   return r

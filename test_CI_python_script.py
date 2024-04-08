6d451afd0b639d2bd10b0688e225e548a3b9a223
magdamd <49106367+magdamd@users.noreply.github.com>
2024-04-08 18:08:10 +0200

    Update test_CI_python_script.py - load True values

--- test_CI_python_script.py
+++ test_CI_python_script.py
@@ -1,9 +1,15 @@
 import CI_CD_test_python_script
+import numpy as np
+
+vector = np.loadtxt("True_values.txt")
+
+value1 = vector[0]
+value2 = vector[1]
 
 class TestPythonScript:
 
     def test_add(self):
-        assert 4 == CI_CD_test_python_script.add(2, 2)
+        assert value1 == CI_CD_test_python_script.add(2, 2)
 
     def test_subtraction(self):
-        assert 2 == CI_CD_test_python_script.subtract(4, 2)
+        assert value2 == CI_CD_test_python_script.subtract(4, 2)

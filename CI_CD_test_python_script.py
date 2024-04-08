1513486a3b6a2ffe6fda0e74df414ee1f332ea6e
magdamd <49106367+magdamd@users.noreply.github.com>
2024-04-08 23:30:53 +0200

    Update CI_CD_test_python_script.py

--- CI_CD_test_python_script.py
+++ CI_CD_test_python_script.py
@@ -2,12 +2,12 @@ import numpy as np
 
 def add(a, b):
   r = a + b
-  with open("New_version_values.txt", "ab") as f:
-    np.savetxt("New_version_values.txt", r, fmt='%1.3f')
+  #with open("New_version_values.txt", "ab") as f:
+  np.savetxt("New_version_values.txt", r, fmt='%1.3f')
   return r
 
 def subtract(a, b):
   r = a - b
-  with open("New_version_values.txt", "ab") as f:
-    np.savetxt("New_version_values.txt", r, fmt='%1.3f')
+  #with open("New_version_values.txt", "ab") as f:
+  np.savetxt("New_version_values.txt", r, fmt='%1.3f')
   return r
